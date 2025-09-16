4269155b319a9a04f8252a1b6f4f02b97f7d95b2
Gracie <graciecuriel7@csub.edu>
2025-09-16 12:39:41 -0700

    Converted to MVC model

---
+++ src/models/StudentData.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentData {
+    private final List<Student> students;
+    private String filename;
+
+    public StudentData(){
+        filename = "./student.data";
+        students = new ArrayList<>();
+    }
+    // Setters
+    public void addStudent(Student student){
+        students.add(student);
+    }
+    public void removeStudent(Student student){
+        students.remove(student);
+    }
+
+    // Getters
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    public void saveData() throws IOException, ClassCastException {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
+        out.writeObject(students);
+    }
+
+    public void loadData() throws IOException, ClassNotFoundException{
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+        List<Student> loaded = (List<Student>) in.readObject();
+        students.clear();
+        students.addAll(loaded);
+    }
+
+
+}
